1bde079e7ef098313fdd573e462c8b80c3f6b43b
21kauadev <kauantnetsoares@gmail.com>
2025-03-03 16:24:01 -0300

    add files

---
+++ src/main/java/com/kauadev/products_crud_springboot/ProductsCrudSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.kauadev.products_crud_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsCrudSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsCrudSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kauadev/products_crud_springboot/ProductsCrudSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kauadev.products_crud_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsCrudSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
